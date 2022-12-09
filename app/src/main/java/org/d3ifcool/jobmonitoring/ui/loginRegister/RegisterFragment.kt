943779e8881e675258c8576c8cf9d0d888081d6c
suryana <suryanallssll@gmail.com>
2022-12-09 16:11:53 +0700

    memunculkan data pekerjaan di karyawan

--- app/src/main/java/org/d3ifcool/jobmonitoring/ui/loginRegister/RegisterFragment.kt
+++ app/src/main/java/org/d3ifcool/jobmonitoring/ui/loginRegister/RegisterFragment.kt
@@ -13,7 +13,6 @@ import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.auth.ktx.userProfileChangeRequest
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import org.d3ifcool.jobmonitoring.R
